43a72225005c868b0b9182747777aff8fefd6f31
alexander68541-beep <alexander68541@gmail.com>
2026-09-26 22:07:50 +0600

    Add files via upload

--- src/components/CrudSection.tsx
+++ src/components/CrudSection.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { apiFetch, ApiError } from "@/lib/api";
